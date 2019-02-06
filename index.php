97d67af60da2949a4d0203ae5e74170b8fadfaf0
Ronald Luna <ronaldluna1@gmail.com>
2019-02-06 14:05:28 -0700

    bootstrap challenge

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>bootstrap</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- CSS style link -->
+		<link rel="stylesheet" href="styles/css-style.css" type="text/css" />
+
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	</head>
+	<body>
+		<section class="top-stuff">
+			<div class="row">
+				<div class="col-12">
+					<div class="container">
+						<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+							<div class="row">
+								<div class="col-8">
+									<a class="navbar nav-brand">Title</a>
+								</div>
+							</div>
+							<button class="navbar-toggler text-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse col-4" id="navbarTogglerDemo01">
+								<ul class="navbar-nav text-right">
+									<li class="nav-item active">
+										<a class="nav-link" href="https://mattjdavid.com/public_html/">link 1 </a>
+									</li>
+									<li class="nav-item active">
+										<a class="nav-link" href="https://www.motosport.com/">link 2</a>
+									</li>
+									<li class="nav-item active">
+										<a class="nav-link" href="https://deepdivecoding.com/">link 3</a>
+									</li>
+									<li class="nav-item active">
+										<a class="nav-link" href="https://albuquerque.craigslist.org/">link 4</a>
+									</li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+			<div class="jumbotron col-11 ml-auto mr-auto">
+				<h1 class="display-3">Hello, world!</h1>
+				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for
+					calling extra attention to featured content or information.</p>
+				<hr class="my-2">
+				<p>It uses utility classes for typography and
+					spacing to space content out within the larger container.</p>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="https://albuquerque.craigslist.org/" role="button">Some action</a>
+				</p>
+			</div>
+		</section>
+
+		<section class="about">
+			<div class="container-fluid bg-light">
+				<div class="row">
+					<div class="col-sm-3 m-2">
+
+					</div>
+					<div class="col-sm-8">
+						<p>Mr frothy-top socially mobile French café patron joseph stalin challenge you to a duel. Mr frothy-top handsome clive dunn glorious facial hair mark lawrenson crumb catcher.
+
+							Mark lawrenson hungarian bruce forsyth albert einstein knavish rogue super mario handlebar, hungarian handlebar knavish rogue burt reynolds consectetur zap rowsdower bruce forsyth super mario albert einstein mark lawrenson ron jeremy. Yosemite sam lady magnets success mustachioed 118 118.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="container">
+			<div class="row">
+				<div class="col-sm-3">
+
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Fancy Linux</h4>
+							<p class="card-text">
+								look how much I care about what Im putting in the card text!! isn't this fun fun fun
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3">
+
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Fancy Linux</h4>
+							<p class="card-text">
+								look how much I care about what Im putting in the card text!! isn't this fun fun fun
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3">
+
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Fancy Linux</h4>
+							<p class="card-text">
+								look how much I care about what Im putting in the card text!! isn't this fun fun fun
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3">
+
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Fancy Linux</h4>
+							<p class="card-text">
+								look how much I care about what Im putting in the card text!! isn't this fun fun fun
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="footer">
+			<div class="container-fluid bg-dark">
+				<p class="align-content-center">howdy howdy howdy howdy</p>
+			</div>
+		</section>
+	</body>
