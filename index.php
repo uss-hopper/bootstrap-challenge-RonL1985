c50b83f23d81327ffc47f35958348ce2b95b6e43
Ronald Luna <ronaldluna1@gmail.com>
2019-02-07 17:31:33 -0700

    bootstrap challenge

--- index.php
+++ index.php
@@ -3,8 +3,113 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Title</title>
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 	</head>
 	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">Navbar</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Features</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Pricing</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled" href="#">Disabled</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
 
+			</div>
+
+		</header>
+		<section class="py-5">
+			<div class="container">
+				<div class="jumbotron">
+					<h1 class="display-4">Hello, world!</h1>
+					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<hr class="my-4">
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+				</div>
+
+			</div>
+
+		</section>
+		<section class="bg-secondary py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="img-fluid d-block mx-auto" src="https://fillmurray.com/200/300" alt="bill murray">
+					</div>
+					<div class="col-sm-9">
+						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab. Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		<section>
+			<div class="container mt-5">
+				<div class="row">
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="https://www.fillmurray.com/300/300" alt="bill murray">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="https://www.fillmurray.com/300/300" alt="bill murray">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="https://www.fillmurray.com/300/300" alt="bill murray">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="https://www.fillmurray.com/300/300" alt="bill murray">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		<footer>
+
+		</footer>
 	</body>
 </html>
