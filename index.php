5393b9287f994b47317de63232e65375dcc5f7bd
Ronald Luna <ronaldluna1@gmail.com>
2019-02-27 11:58:33 -0700

    bootstrap challenge changes

--- index.php
+++ index.php
@@ -63,8 +63,12 @@
 			</div>
 
 			<div class="content-wrapper mt-3">
-				<p class="">Spicy jalapeno bacon beef sausage.</p>
-				<p>Hamburger sausage bacon, beef hicken turkey.</p>
+				<p class="">Yeah, I like animals better than people sometimes... Especially dogs.
+					Dogs are the best. Every time you come home, they act like they haven't seen you in a year.
+					And the good thing about dogs... is they got different dogs for different people. Like pit bulls.
+					The dog of dogs. Pit bull can be the right man's best friend... or the wrong man's worst enemy.
+					You going to give me a dog for a pet, give me a pit bull. Give me... Raoul. Right, Omar? Give me Raoul.</p>
+
 			</div>
 		</div>
 
